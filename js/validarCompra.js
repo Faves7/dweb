eaa6e1364310b8c7dee3520ef2f7fd0a9a34ab85
Favez <facuchaves957@gmail.com>
2021-07-06 17:06:59 -0300

    Modificaciones en la venta

--- js/validarCompra.js
+++ js/validarCompra.js
@@ -4,17 +4,20 @@ function validarCompra(){
     var direccion = document.form.dir.value;
     var formaPago = document.form.formaPago.value;
 
-    if( nombre.length == 0 || nroDocumento.length == 0 ){
+    if ( nombre.length == 0 || nroDocumento.length == 0 || direccion.length == 0){
         alert("Debe completar los campos solictados, por favor");
     }else{
         var mensaje = "Nombre y Apellido: " + nombre + "\r\n";
         mensaje += "Número de Documento: " + nroDocumento + "\r\n";
-        if(formaPago == "1"){
-            mensaje += "FOrma de Pago: Efectivo" + "\r\n";
-            mensaje += "Dirección: " + direccion;
+        mensaje += "Dirección: " + direccion + "\r\n";
+        if( formaPago == "1" ){
+            mensaje += "Forma de pago: Efectivo";
         }else{
-            mensaje += "Forma de Pago: Mercado Pago" + "\r\n";
+            mensaje += "Forma de pago: Mercado Pago";
         }
         window.print(mensaje);
     }
 }
+
+//Falta agregar a la variable mensaje el campo seleccionado del medio de pago
+//Falta: En caso de que se seleccione la opción "Punto de encuentro", deshabilitar la casilla "Domicilio". 
